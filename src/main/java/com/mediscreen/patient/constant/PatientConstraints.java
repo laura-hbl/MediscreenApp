d5d4e587e6f4f4d5dbc11ed60556582b733f1c5e
Laura <laura_habdul@hotmail.fr>
2021-06-08 00:43:03 +0200

    Adds patient constraints

---
+++ src/main/java/com/mediscreen/patient/constant/PatientConstraints.java
@@ -0,0 +1,15 @@
+package com.mediscreen.patient.constant;
+
+public class PatientConstraints {
+
+    public static final int FIRST_NAME_MAX_SIZE = 125;
+
+    public static final int LAST_NAME_MAX_SIZE = 125;
+
+    public static final int SEX_MAX_SIZE = 1;
+
+    public static final int ADDRESS_MAX_SIZE = 150;
+
+    public static final int PHONE_MAX_SIZE = 20;
+}
+
