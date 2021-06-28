cefa8b218a921804da14cd1fd06e1f73c7e2e3bd
Laura <laura_habdul@hotmail.fr>
2021-06-28 21:00:02 +0200

    Creates request to retrieves a patient note list, delete a note and updates pom.xml file

--- patientHistory/src/main/java/com/mediscreen/patientHistory/controller/NoteController.java
+++ patientHistory/src/main/java/com/mediscreen/patientHistory/controller/NoteController.java
@@ -1,11 +1,14 @@
 package com.mediscreen.patientHistory.controller;
 
 import com.mediscreen.patientHistory.dto.NoteDTO;
-import com.mediscreen.patientHistory.service.INotesService;
+import com.mediscreen.patientHistory.service.INoteService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/note")
@@ -13,15 +16,15 @@ public class NoteController {
 
     private static final Logger LOGGER = LogManager.getLogger(NoteController.class);
 
-    private final INotesService notesService;
+    private final INoteService notesService;
 
     @Autowired
-    public NoteController(final INotesService notesService) {
+    public NoteController(final INoteService notesService) {
         this.notesService = notesService;
     }
 
-
     @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
     public NoteDTO addNote(@RequestBody final NoteDTO noteDTO) {
         LOGGER.debug("POST Request on /note/add with patient id : {}", noteDTO.getPatientId());
 
@@ -31,13 +34,13 @@ public class NoteController {
         return noteAdded;
     }
 
-    @GetMapping("/update/{id}")
+    @GetMapping("/get/{id}")
     public NoteDTO getNoteById(@PathVariable("id") final String noteId) {
-        LOGGER.debug("GET Request on /note/update/{id} with note id : {}", noteId);
+        LOGGER.debug("GET Request on /note/get/{id} with note id : {}", noteId);
 
         NoteDTO noteFound = notesService.getNoteById(noteId);
 
-        LOGGER.info("GET Request on /note/update/{id} - SUCCESS");
+        LOGGER.info("GET Request on /note/get/{id} - SUCCESS");
         return noteFound;
     }
 
@@ -49,5 +52,24 @@ public class NoteController {
 
         LOGGER.info("POST Request on /note/update/{id} - SUCCESS");
         return noteUpdated;
+    }
+
+    @GetMapping("/list/{id}")
+    public List<NoteDTO> getNoteList(@PathVariable("id") final Integer patientId) {
+        LOGGER.debug("GET Request on /note/list/{id} with patient id : {}", patientId);
+
+        List<NoteDTO> allNote = notesService.getAllNote(patientId);
+
+        LOGGER.info("GET Request on /note/list/{id} - SUCCESS");
+        return allNote;
+    }
+
+    @GetMapping("/delete/{id}")
+    public void deleteNote(@PathVariable("id") final String noteId) {
+        LOGGER.debug("GET Request on /note/delete/{id} with note id : {}", noteId);
+
+        notesService.deleteNote(noteId);
+
+        LOGGER.info("GET Request on /note/delete/{id} - SUCCESS");
     }
 }
