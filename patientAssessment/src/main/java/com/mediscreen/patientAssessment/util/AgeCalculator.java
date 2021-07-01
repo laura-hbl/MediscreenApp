d01d9b42507855d18c3e344ba5954cd25c69c587
Laura <laura_habdul@hotmail.fr>
2021-07-01 19:35:50 +0200

    Adds AgeCalculator util class

---
+++ patientAssessment/src/main/java/com/mediscreen/patientAssessment/util/AgeCalculator.java
@@ -0,0 +1,44 @@
+package com.mediscreen.patientAssessment.util;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+/**
+ * Calculates a patient'age.
+ *
+ * @author Laura Habdul
+ */
+@Component
+public class AgeCalculator {
+
+    /**
+     * AgeCalculator logger.
+     */
+    private static final Logger LOGGER = LogManager.getLogger(AgeCalculator.class);
+
+    /**
+     * Calculates a patient'age from their date of birth.
+     *
+     * @param birthDate birth date of the patient
+     * @return the patient's age after calculation.
+     */
+    public int getAge(final LocalDate birthDate) {
+
+        LocalDate currentDate = LocalDate.now();
+        int age = Period.between(birthDate, currentDate).getYears();
+
+        if (birthDate.isAfter(currentDate)) {
+            LOGGER.error("BirthDate provided is incorrect:" + birthDate);
+            throw new IllegalArgumentException("Invalid: birthDate is in the future");
+        } else if (age == 0) {
+            LOGGER.debug("Baby with less than one year");
+            age++;
+        }
+
+        return age;
+    }
+}
