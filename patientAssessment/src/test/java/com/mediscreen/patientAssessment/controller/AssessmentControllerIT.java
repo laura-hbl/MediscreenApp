f36d4b3d0348fa40d75028db8690378be98c01c0
Laura <laura_habdul@hotmail.fr>
2021-07-07 22:44:50 +0200

    Adds Dockerfile in microservices

--- patientAssessment/src/test/java/com/mediscreen/patientAssessment/controller/AssessmentControllerIT.java
+++ patientAssessment/src/test/java/com/mediscreen/patientAssessment/controller/AssessmentControllerIT.java
@@ -11,12 +11,14 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource({"/application-test.properties"})
 public class AssessmentControllerIT {
 
     @Autowired
